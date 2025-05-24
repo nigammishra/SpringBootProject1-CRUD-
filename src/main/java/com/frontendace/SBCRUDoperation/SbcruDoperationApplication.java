6834a3a375c1e2f53b13bc67fa82b2fa0d7f4b18
nigam <nigammishra826@gmail.com>
2025-05-24 23:03:32 +0530

    first commit

---
+++ src/main/java/com/frontendace/SBCRUDoperation/SbcruDoperationApplication.java
@@ -0,0 +1,13 @@
+package com.frontendace.SBCRUDoperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbcruDoperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbcruDoperationApplication.class, args);
+	}
+
+}
